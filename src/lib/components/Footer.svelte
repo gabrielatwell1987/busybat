41970097b9da45515eb52dc100543aa70d5843b5
gabrielatwell1987 <gabrielatwell@proton.me>
2025-03-09 13:58:59 -0700

    fixed the footer text and scale for mobile

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <footer>
-	<p>&copy; {year} {name}.</p>
+	<p><span>&copy;</span> {year} <span>{name}</span> Co. All rights reserved</p>
 </footer>
 
 <style>
@@ -18,17 +18,33 @@
 		border-radius: var(--radius);
 		height: 3em;
 		z-index: 20;
+		max-width: calc(100% - 2rem);
+		width: auto;
 
 		& p {
 			font-family: var(--font-semibold);
 			color: var(--color-secondary);
 			padding: 0 0.5rem;
-			font-size: clamp(var(--sm), 2vw, var(--h4));
+			font-size: clamp(var(--xs), 1vw, var(--sm));
 			font-weight: 400;
 			text-wrap: nowrap;
 			display: flex;
 			justify-content: center;
 			align-items: center;
+
+			& span {
+				margin-top: 0.215rem;
+
+				&:nth-child(2) {
+					color: var(--color-accent);
+					padding-inline: 0.5rem;
+					margin-top: 0;
+				}
+			}
+
+			@media (max-width: 500px) {
+				scale: 0.9;
+			}
 		}
 	}
 </style>
