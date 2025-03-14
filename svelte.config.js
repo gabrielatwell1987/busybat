afa7332ac6c3e2dc65c4f5e4465aba16a07be690
gabrielatwell1987 <gabrielatwell@proton.me>
2025-03-14 13:50:49 -0700

    changed back

--- svelte.config.js
+++ svelte.config.js
@@ -5,12 +5,9 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter()
-	},
-	compilerOptions: {
-		runes: true
-	}
+	adapter: adapter({
+		external: []
+	})
 };
 
 export default config;
