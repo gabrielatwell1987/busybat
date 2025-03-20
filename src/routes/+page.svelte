576c2997213295ca659891a49d37130b963a0f61
gabrielatwell1987 <gabrielatwell@proton.me>
2025-03-20 13:00:54 -0700

    added some keywords and a better seo description for main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,8 @@
 
 <SEO
 	title="Busy Little Bat Sewing Co."
-	description="Handmade bags and accessories"
-	keywords="busy bat, busy bat sewing, busy bat sewing co"
+	description="Handmade sewing company in Las Vegas, Nevada. Specializing in bags and accessories."
+	keywords="busy bat, busy bat sewing, busy bat sewing co, busy bat sewing company, busy bat las vegas, busy bat sewing las vegas, busy bat sewing co las vegas, handmade bags, handmade accessories, handmade bags las vegas, handmade accessories las vegas"
 />
 
 <MainLogo />
