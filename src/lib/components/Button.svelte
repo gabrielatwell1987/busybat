28f2b00cdb7ccd65b0b47824de2f89608dc1efe4
gabrielatwell1987 <gabrielatwell@proton.me>
2025-04-01 14:16:57 -0700

    on Button i added an newTab prop to open a link in a new tab, and I did a full redo of /about with the content lollie wanted and added the pin-leo as a background

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,77 +1,16 @@
-<!-- <script>
-	import { goto } from '$app/navigation';
-
-	let { title, href } = $props();
-
-	function handleClick(e) {
-		e.preventDefault();
-
-		goto(href);
-	}
-</script>
-
-<a {href} class="button">
-	{title}
-</a>
-
-<style>
-	a.button {
-		font-family: var(--font-semibold);
-		font-size: clamp(var(--h6), 2vw, var(--h3));
-		padding: 0.5rem 1rem;
-		background-color: var(--color-accent);
-		color: var(--color-primary);
-		border: none;
-		border-radius: var(--radius);
-		cursor: pointer;
-		width: fit-content;
-		text-decoration: none;
-		display: inline-block;
-		text-align: center;
-		position: relative;
-		z-index: 50;
-
-		&:hover {
-			background: linear-gradient(
-				70deg,
-				var(--color-accent),
-				var(--color-secondary),
-				var(--color-accent)
-			);
-			background-size: 200% 100%;
-			box-shadow: 0 0 5px var(--color-accent);
-			animation: gradientFlow 3s ease infinite;
-		}
-
-		@media (width <= 500px) {
-			font-size: clamp(var(--sm), 2vw, var(--h4));
-		}
-	}
-
-	@keyframes gradientFlow {
-		0% {
-			background-position: 0% 50%;
-		}
-		50% {
-			background-position: 100% 50%;
-		}
-		100% {
-			background-position: 0% 50%;
-		}
-	}
-</style>
-
- -->
-
 <script>
 	import { browser } from '$app/environment';
 	import { goto } from '$app/navigation';
 
-	let { title, href } = $props();
+	let { title, href, newTab = false } = $props();
 
 	function handleClick() {
 		if (browser) {
-			window.location.href = href;
+			if (newTab) {
+				window.open(href, '_blank');
+			} else {
+				window.location.href = href;
+			}
 		}
 	}
 </script>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,8 +2,7 @@
 	import SEO from '$lib/data/SEO.svelte';
 	import VerticalTitle from '$lib/components/VerticalTitle.svelte';
 	import Image from '$lib/components/Image.svelte';
-	import card from '$lib/assets/bcard-front.webp';
-	import leopard from '$lib/assets/leopard.webp';
+	import Button from '$lib/components/Button.svelte';
 </script>
 
 <SEO
@@ -12,76 +11,180 @@
 	keywords="about busy little bat sewing, about busy little bat"
 />
 
+<div class="background-wrapper"></div>
+
 <main>
 	<VerticalTitle title="About" />
 
 	<section class="content">
-		<h5>Busy Little Bat Sewing</h5>
-
-		<h2>About us</h2>
+		<h2>Welcome to</h2>
+		<h2>Busy Little Bat Sewing</h2>
+		<h2>where dark meets delightful in every stitch.</h2>
 
 		<article class="summary">
-			<p class="width indent">I make custom clothing and accessories for all ages and sizes.</p>
+			<p class="width indent">
+				I am a bewitchingly uncommon spirit with a haunting passion for arts and sewing. My name is
+				Adrienne and I am the busiest of all bats. With a background steeped in the mysteries of
+				sewing, design, and art. I strive to bring the hauntingly beautiful to everything I create.
+			</p>
 
 			<p class="width indent">
-				I specialize in unique and fun designs. I are a small business that is dedicated to
-				providing quality products and services to our customers. I take pride in our work and
-				strive to make every customer happy. I are very passionate about sewing and love sharing our
-				knowledge with others. I are committed to providing a positive and welcoming environment for
-				all of our customers. We look forward to working with you!
+				My journey began many dark nights ago with a passion for art, music and, quiet frankly,
+				clothes that actually fit. As a self-trained sewist, I unearthed my passion for creating
+				unique items that fit my goth/punk aesthetic. Over the years, I have sharpened my skills
+				like a finely honed blade, gaining invaluable experience through various ghostly projects
+				and eerie collaborations. Each step along the way has transformed me into the ghoul that I
+				am today.
 			</p>
 
-			<p class="width">
-				If you have any questions or would like to learn more about our products and services,
-				please feel free to contact us. I am always happy to help and look forward to hearing from
-				you!
+			<p class="width indent">
+				I specialize in upcycling old denim, flannels and t-shirts into items for those who dare to
+				stand out. Each stitch weaves a web of gothic glamour, gutter-punk horror and eerie every
+				day wear. My creations are made with a touch of magic and a whole lot of love. I create
+				sustainable, wearable art the blends the macabre with sustainability proving that fashion
+				can be both distinctive and kind to the planet. I believe that each ghastly creature on this
+				planet has the power to change lives and bring about unsettlingly meaningful change.
 			</p>
+			<p class="width indent">
+				Do you hear the whispers of the night calling? Whether you seek a custom creation, have a
+				question from beyond, or simply wish to share your love for the eerie and elegant, I am but
+				a message away. I welcome all kindred spirits, fellow night dwellers, and lovers of the
+				beautifully macabre.
+			</p>
+
+			<div class="contact-me">
+				<p class="width"><span>📜</span> Send a Raven (or email):</p>
+
+				<Button title="Summon Me" href="/contact" />
+			</div>
+
+			<div class="contact-me">
+				<p class="width"><span>📲</span> Echoes in the Void:</p>
+
+				<Button
+					title="Instagram"
+					href="https://www.instagram.com/busy_little_bat_sewing/"
+					newTab={true}
+				/>
+			</div>
 		</article>
-
-		<Image src={card} alt="Busy Little Bat Sewing" width="auto" />
 	</section>
 </main>
 
 <style>
+	.background-wrapper {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		opacity: 0.2;
+		z-index: 5;
+		background-image: url(/photos/pink-leo.webp);
+		background-repeat: repeat;
+		background-size: cover;
+		background-position: center;
+		background-attachment: fixed;
+	}
+
 	main {
 		height: auto;
+		position: relative;
+		z-index: 6;
+		width: 100%;
+		padding: 0;
+		margin: 0;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 
 		& .content {
 			display: grid;
 			place-items: center;
-			padding-top: 3em;
 			margin-bottom: 5em;
-			padding-top: 3em;
 			view-transition-name: slide-content;
+			width: 100%;
+			max-width: 1500px;
+			padding: 0;
+			box-sizing: border-box;
+
+			@media (width <= 500px) {
+				margin-inline: auto;
+				padding: 0;
+				max-width: 100%;
+			}
 
 			& h2 {
 				font-family: var(--font-title);
-				font-size: clamp(var(--h4), 15vw, var(--xxl));
+				font-size: clamp(var(--h6), 5vw, var(--h1));
 				font-weight: 700;
-				letter-spacing: 5px;
 				margin: 0;
 				padding: 0;
 				color: var(--color-accent);
 				-webkit-text-stroke: 1px var(--color-secondary);
 				text-shadow: 2px 4px 4px rgba(100, 100, 100, 0.35);
-			}
+				width: 75%;
+				margin-inline: auto;
+				text-align: center;
+				width: 100%;
+				max-width: 800px;
 
-			& h5 {
-				font-family: var(--font-regular);
-				font-size: clamp(var(--sm), 1.5vw, var(--h6));
-				font-weight: 700;
-				font-style: italic;
-				margin: 0;
-				padding: 0;
-				margin-bottom: -1em;
+				@media (width <= 768px) {
+					width: 100%;
+				}
 
-				@media (width <= 850px) {
-					margin-bottom: -0.5em;
+				&:nth-of-type(2) {
+					margin-top: -0.5em;
+				}
+
+				&:nth-of-type(3) {
+					line-height: 1.35;
+					font-family: var(--font-semibold);
+					font-size: clamp(var(--xs), 3vw, var(--h5));
+					font-weight: 300;
+					color: var(--color-secondary);
+					-webkit-text-stroke: none;
+					text-shadow: none;
 				}
 			}
 
 			& .summary {
 				margin-top: 1em;
+				width: 100%;
+				max-width: 1500px;
+				padding: 0 1rem;
+				box-sizing: border-box;
+
+				& .contact-me {
+					display: flex;
+					gap: 1rem;
+					align-items: center;
+					justify-content: center;
+					margin-bottom: 1rem;
+					width: 100%;
+					flex-wrap: wrap;
+
+					&:first-of-type {
+						margin-top: 2em;
+					}
+
+					@media (width <= 500px) {
+						padding: 0 0.5rem;
+					}
+
+					& p.width {
+						max-width: unset;
+						width: auto;
+						margin: 0;
+						white-space: nowrap;
+						flex-shrink: 0;
+
+						& span {
+							font-size: clamp(var(--sm), 4vw, var(--h1));
+							opacity: 0.8;
+						}
+					}
+				}
 
 				& p {
 					margin-bottom: 0.15rem;
@@ -93,27 +196,16 @@
 					&.width {
 						max-width: 1500px;
 						width: 100%;
+						padding: 0 1rem;
+						box-sizing: border-box;
 						margin-inline: auto;
 						text-align: left;
-						font-size: clamp(var(--sm), 4vw, var(--h5));
+						font-size: clamp(var(--sm), 3vw, var(--h5));
 						font-weight: 500;
 
-						&:first-child {
-							text-align: center;
-							font-family: var(--font-bold);
-							font-size: clamp(var(--h5), 5vw, var(--h1));
-							font-weight: 200;
-							max-width: 1200px;
-							width: fit-content;
-							line-height: 1.25;
-							margin-bottom: 1.5em;
+						@media (width <= 500px) {
 							padding: 0;
-							text-wrap: balance;
-							color: var(--color-dark);
-						}
-
-						&:nth-child(2) {
-							font-size: clamp(var(--h6), 6vw, var(--h4));
+							max-width: 100%;
 						}
 
 						&:nth-child(3) {
@@ -124,6 +216,8 @@
 
 							@media (width <= 500px) {
 								letter-spacing: 0;
+								padding: 0;
+								max-width: 100%;
 							}
 						}
 					}
