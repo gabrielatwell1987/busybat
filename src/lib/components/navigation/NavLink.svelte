90f083a80b16cceeebc09a340ee9fe58a7e6b528
gabrielatwell1987 <gabrielatwell@proton.me>
2025-07-04 13:05:14 -0700

    changed NavLink to use scaling instead of flipping

--- src/lib/components/navigation/NavLink.svelte
+++ src/lib/components/navigation/NavLink.svelte
@@ -5,14 +5,12 @@
 	let isCurrentPage = $derived($page.url.pathname === url);
 </script>
 
-<a class="quick-flip" href={url} {onclick} aria-current={isCurrentPage ? 'page' : undefined}
+<a class="nav-link" href={url} {onclick} aria-current={isCurrentPage ? 'page' : undefined}
 	>{title}</a
 >
 
 <style>
-	.quick-flip {
-		--transition-duration: 0.4s;
-		--timing-function: cubic-bezier(0.66, 0, 0.34, 1);
+	.nav-link {
 		overflow-y: clip;
 		list-style: none;
 		margin: 0;
@@ -27,16 +25,10 @@
 		margin-right: clamp(0.25rem, 1vw, 0.5rem);
 		white-space: nowrap;
 		text-align: center;
+		transition: scale 0.2s ease;
 
 		@media (600px <= width <= 768px) {
 			font-size: clamp(var(--sm), 2.5vw, var(--h5));
-		}
-
-		@media (width >= 501px) {
-			transition:
-				transform var(--transition-duration) var(--timing-function),
-				translate 0ms calc(var(--transition-duration) / 2),
-				color 0.3s ease;
 		}
 
 		@media (width <= 500px) {
@@ -46,11 +38,7 @@
 		&:is(:hover, :focus) {
 			color: var(--color-accent);
 			-webkit-text-stroke: 1px var(--color-secondary);
-
-			@media (min-width: 501px) {
-				transform: translateY(-2lh);
-				translate: 0 2lh;
-			}
+			scale: 1.05;
 		}
 
 		&:focus {
